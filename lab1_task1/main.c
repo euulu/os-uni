24491627725eb1966bb0f58d051f002afdf554a1
anegin <anegamega@gmail.com>
2022-12-05 20:09:21 +0200

    Lab1: task1 finish p1-p2-p3 processes hierarchy.

--- lab1_task1/main.c
+++ lab1_task1/main.c
@@ -3,11 +3,15 @@
 #include "sys/wait.h"
 
 int main() {
-    pid_t pid1;
+    pid_t pid1, pid2;
 
     pid1 = fork();
 
     if (pid1 == 0) {
+        pid2 = fork();
+        if (pid2 == 0) {
+            sleep(20);
+        }
         sleep(20);
     } else {
         waitpid(pid1, NULL, 0);
