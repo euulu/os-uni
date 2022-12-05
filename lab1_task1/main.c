1d34836986d919a40852b04dae0658f754fc26a8
anegin <anegamega@gmail.com>
2022-12-05 20:02:10 +0200

    Lab1: task1 add first level child process.

--- lab1_task1/main.c
+++ lab1_task1/main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "stdio.h"
+#include "sys/wait.h"
 
 int main() {
-    printf("Hello, World!\n");
+    pid_t pid1;
+
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        sleep(20);
+    } else {
+        waitpid(pid1, NULL, 0);
+        printf("Done.");
+    }
+
     return 0;
 }
