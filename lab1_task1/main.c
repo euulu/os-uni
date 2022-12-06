31318505c725e1e627213efab2ed4cf965526471
anegin <anegamega@gmail.com>
2022-12-06 21:23:37 +0200

    Lab1: task1 add three first level children processes.

--- lab1_task1/main.c
+++ lab1_task1/main.c
@@ -3,19 +3,24 @@
 #include "sys/wait.h"
 
 int main() {
-    pid_t pid1, pid2;
+    pid_t pid1, pid2, pid3;
 
     pid1 = fork();
 
     if (pid1 == 0) {
+        sleep(10);
+    } else {
         pid2 = fork();
         if (pid2 == 0) {
-            sleep(20);
+            sleep(10);
+        } else {
+            pid3 = fork();
+            if (pid3 == 0) {
+                sleep(10);
+            } else {
+                waitpid(pid1, NULL, 0);
+            }
         }
-        sleep(20);
-    } else {
-        waitpid(pid1, NULL, 0);
-        printf("Done.");
     }
 
     return 0;
