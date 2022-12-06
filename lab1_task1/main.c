9102eeef58e2d7feec47ee94f7aa9af34794f5bc
anegin <anegamega@gmail.com>
2022-12-06 21:37:03 +0200

    Lab1(task1): refactor(remove unneded variables, change variable names).

--- lab1_task1/main.c
+++ lab1_task1/main.c
@@ -2,26 +2,26 @@
 #include "sys/wait.h"
 
 int main() {
-    pid_t pid1, pid2, pid3, pid4, pid5;
+    pid_t pid1, pid2, pid3, pid4;
 
     pid1 = fork();
 
     if (pid1 == 0) {
-        pid4 = fork();
+        fork();
         sleep(10);
     } else {
         pid2 = fork();
         if (pid2 == 0) {
-            pid4 = fork();
-            if (pid4 == 0) {
+            pid3 = fork();
+            if (pid3 == 0) {
                 sleep(10);
             } else {
-                pid5 = fork();
+                fork();
                 sleep(10);
             }
         } else {
-            pid3 = fork();
-            if (pid3 == 0) {
+            pid4 = fork();
+            if (pid4 == 0) {
                 sleep(10);
             } else {
                 waitpid(pid1, NULL, 0);
