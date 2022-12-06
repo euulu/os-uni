16c5d846778bb1ed3f44e4d82d92ec65f1c8dbe4
anegin <anegamega@gmail.com>
2022-12-06 21:34:11 +0200

    Lab1(task1): build complete processes tree for the first variant.

--- lab1_task1/main.c
+++ lab1_task1/main.c
@@ -1,18 +1,24 @@
 #include <unistd.h>
-#include "stdio.h"
 #include "sys/wait.h"
 
 int main() {
-    pid_t pid1, pid2, pid3;
+    pid_t pid1, pid2, pid3, pid4, pid5;
 
     pid1 = fork();
 
     if (pid1 == 0) {
+        pid4 = fork();
         sleep(10);
     } else {
         pid2 = fork();
         if (pid2 == 0) {
-            sleep(10);
+            pid4 = fork();
+            if (pid4 == 0) {
+                sleep(10);
+            } else {
+                pid5 = fork();
+                sleep(10);
+            }
         } else {
             pid3 = fork();
             if (pid3 == 0) {
