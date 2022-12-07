bd28ffcafa03b8951b7511631952b22ef702b9e0
anegin <anegamega@gmail.com>
2022-12-07 14:48:35 +0200

    Lab1(task2): handle no args case.

--- lab1_task2/main.c
+++ lab1_task2/main.c
@@ -3,6 +3,11 @@
 #include "sys/wait.h"
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Nothing to execute. Please enter some arguments next time.");
+        return 0;
+    }
+
     pid_t pid = fork();
     if (pid == 0) {
         printf("I am a child process: %d\n", getppid());
