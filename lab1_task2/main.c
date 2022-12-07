8b26cacd0e5b960cf7c287d1779b67eb5fd282cb
anegin <anegamega@gmail.com>
2022-12-07 14:36:37 +0200

    Lab1(task2): create(fork) process to execute passed command in it.

--- lab1_task2/main.c
+++ lab1_task2/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/wait.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        printf("I am a child process: %d\n", getppid());
+    } else {
+        waitpid(pid, NULL, 0);
+        printf("I am a parent process: %d\n", getppid());
+    }
+
     return 0;
 }
