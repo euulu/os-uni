01dfe669917048e5b1fbce0f00602acdc285b8ee
anegin <anegamega@gmail.com>
2023-01-30 17:36:46 +0200

    Lab1(task2): reformat includes.

--- lab1/task2/main.c
+++ lab1/task2/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
-#include "stdio.h"
-#include "unistd.h"
-#include "sys/wait.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
